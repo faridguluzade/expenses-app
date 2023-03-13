e3de5da28ec225a8baa4e61221766e418960c156
Farid <faridguluzade26@gmail.com>
2023-03-13 15:52:26 -0800

    makin child to parent communication and ExpenseFilter component added

--- src/components/NewExpense/NewExpense.jsx
+++ src/components/NewExpense/NewExpense.jsx
@@ -1,12 +1,22 @@
+import Card from "../UI/Card";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = ({ onAddExpense }) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+
+    onAddExpense(expenseData);
+  };
+
   return (
-    <div className="new-expense">
-      <ExpenseForm />
-    </div>
+    <Card className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </Card>
   );
 };
 
